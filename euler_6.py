1cc92fe9713879c1fe0c473b24dc7e1507ea8867
ManZoor <manzoor_81@hotmail.com>
2013-12-05 00:30:33 -0800

    Euller project

--- euler_6.py
+++ euler_6.py
@@ -12,7 +12,6 @@ natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
-
 '''
 
 current_power=0
